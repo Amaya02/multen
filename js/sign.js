bd1d3d8f1a9cda0980051273fcea1dd1d000622b
CandyAmayaLelis <34389354+Amaya02@users.noreply.github.com>
2018-01-29 18:15:48 +0800

    Update sign.js

--- js/sign.js
+++ js/sign.js
@@ -1,5 +1,5 @@
 function empty() {
-    var x,y;
+    var x,y,z,a,b,c;
     x = document.getElementById("email").value;
     y= document.getElementById("pass").value;
     z = document.getElementById("company").value;
@@ -16,6 +16,19 @@ function empty() {
     $('#demo-2').modal('show');
 }
 
+function empty1() {
+    var x,y;
+    x = document.getElementById("email").value;
+    y= document.getElementById("pass").value;
+	
+    if (x == "" || y =="") {
+        alert("Please fill out all fields");
+      	return false;
+    };
+	alert("Sign up completed");
+    $('#demo-1').modal('hide');
+}
+
 function emptyPayment(){
 	var d,e;
 	d = document.getElementById("paypalemail").value;
@@ -30,96 +43,12 @@ function emptyPayment(){
    	$('#demo-2').modal('hide');
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function empty1() {
-    var y;
-    y= document.getElementById("pass").value;
-    if (y== "") {
-        alert("Enter a valid password");
-        return false;
-
-    };
-    $('#demo-1').modal('hide');
-    $('#demo-2').modal('show');
-}
-
-function empty2() {
-    var z;
-    z = document.getElementById("company").value;
-    if (z == "") {
-        alert("Enter your company");
-        return false;
-    };
-    $('#demo-1').modal('hide');
-    $('#demo-2').modal('show');
-}
-
-function empty3() {
-    var a;
-    a = document.getElementById("address").value;
-    if (a == "") {
-        alert("Invalid address");
-        return false;
-    };
-    $('#demo-1').modal('hide');
-    $('#demo-2').modal('show');
-}
-
-function empty4() {
-    var b;
-    b = document.getElementById("zipcode").value;
-    if (b == "") {
-        alert("Invalid zipcode");
-        return false;
-    };
-    $('#demo-1').modal('hide');
-    $('#demo-2').modal('show');
-}
-
-function empty5() {
-    var c;
-    c = document.getElementById("contact").value;
-    if (c == "") {
-        alert("Invalid contact#");
-        return false;
-    };
-    $('#demo-1').modal('hide');
-    $('#demo-2').modal('show');
+function myFunction(){
+	var x=document.getElementById("pass");
+	if(x.type == "password"){
+			x.type="text";
+	}
+	else{
+		x.type="password";
+	}
 }
