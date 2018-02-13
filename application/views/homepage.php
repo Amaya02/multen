cb6e039f40933dc89be0aa5ef7c434ccd518225b
Harvey Revillas <keikirishima891@gmail.com>
2018-02-13 17:39:43 +0800

    Update homepage.php
    added signup modal

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -11,28 +11,30 @@
     <title>MULTEN - Multi-tenant Man Power Agency</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="assets/vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="<?php echo base_url('assets/vendor/bootstrap/css/bootstrap.css'); ?>" rel="stylesheet">
 	
     <!-- Custom fonts for this template -->
-    <link href="assets/vendor/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url('assets/vendor/font-awesome/css/font-awesome.css'); ?>" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
 	
     <!-- Custom styles for this template -->
-    <link href="assets/css/agency.css" rel="stylesheet">
-    <link href="assets/css/signin.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="assets/css/signup.css">
+	<link type="text/css" href="<?php echo base_url('assets/css/agency.css'); ?>" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/sign.css'); ?>">
+	
+	
 	
 
   </head>
 
   <body id="page-top">
+  
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
       <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="assets/img/logos/logo.jpg" alt=""></a>
+        <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="<?php echo base_url('assets/img/logos/logo.jpg'); ?>" alt=""></a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
           <i class="fa fa-bars"></i>
@@ -120,7 +122,8 @@
 				<div class="col-lg-6">
 					<div class="about-col">
 					<div class="img">
-						 <img class="mx-auto rounded-circle" src="assets/img/about/mission.jpg" alt="">
+						 <img class="mx-auto rounded-circle" src="<?php echo base_url('assets/img/about/mission.jpg'); ?>" alt="">
+						 
 					</div>
 						<p class="text-muted">MULTEN aims to help every man power agency to keep their record and make it safe. It can also provide website for the agencies who doesn't have one. </p>
 				    </div>
@@ -128,7 +131,7 @@
 				<div class="col-lg-6">
 					<div class="about-col">
 					<div class="img">
-						 <img class="mx-auto rounded-circle" src="assets/img/about/vision.jpg" alt="">
+						 <img class="mx-auto rounded-circle" src="<?php echo base_url('assets/img/about/vision.jpg'); ?>" alt="">
 					</div>
 						<p class="text-muted">Enlightening our clients' future through providing exquisite solutions by way of our passion for innovation along with prolonged services</p>
 				    </div>
@@ -150,7 +153,7 @@
         <div class="row">
           <div class="col-sm-4">
             <div class="team-member">
-              <img class="mx-auto rounded-circle" src="assets/img/team/Team1.jpg" alt="">
+              <img class="mx-auto rounded-circle" src="<?php echo base_url('assets/img/team/Team1.jpg'); ?>" alt="">
               <h4>Sophia Barbon</h4>
               <p class="text-muted">Quality Assurance Tester</p>
               <ul class="list-inline social-buttons">
@@ -174,7 +177,7 @@
           </div>
 		  <div class="col-sm-4">
             <div class="team-member">
-              <img class="mx-auto rounded-circle" src="assets/img/team/Team2.jpg" alt="">
+              <img class="mx-auto rounded-circle" src="<?php echo base_url('assets/img/team/Team2.jpg'); ?>" alt="">
               <h4>Hannah Mae Colopano</h4>
               <p class="text-muted">Front-end Developer</p>
               <ul class="list-inline social-buttons">
@@ -198,7 +201,7 @@
           </div>
 		  <div class="col-sm-4">
             <div class="team-member">
-              <img class="mx-auto rounded-circle" src="assets/img/team/Team3.jpg" alt="">
+              <img class="mx-auto rounded-circle" src="<?php echo base_url('assets/img/team/Team3.jpg'); ?>" alt="">
               <h4>Dean Jagmis</h4>
               <p class="text-muted">Back-end Developer</p>
               <ul class="list-inline social-buttons">
@@ -222,7 +225,7 @@
           </div>
 		  <div class="col-sm-4">
             <div class="team-member">
-              <img class="mx-auto rounded-circle" src="assets/img/team/Team4.jpg" alt="">
+              <img class="mx-auto rounded-circle" src="<?php echo base_url('assets/img/team/Team4.jpg'); ?>" alt="">
               <h4>Candy Amaya Lelis</h4>
               <p class="text-muted">Back-end Developer</p>
               <ul class="list-inline social-buttons">
@@ -246,7 +249,7 @@
           </div>
 		  <div class="col-sm-4">
             <div class="team-member">
-              <img class="mx-auto rounded-circle" src="assets/img/team/Team5.jpg" alt="">
+              <img class="mx-auto rounded-circle" src="<?php echo base_url('assets/img/team/Team5.jpg'); ?>" alt="">
               <h4>Rozalyn Oledan</h4>
               <p class="text-muted">Quality Assurance Tester</p>
               <ul class="list-inline social-buttons">
@@ -270,7 +273,7 @@
           </div>
           <div class="col-sm-4">
             <div class="team-member">
-              <img class="mx-auto rounded-circle" src="assets/img/team/Team6.jpg" alt="">
+              <img class="mx-auto rounded-circle" src="<?php echo base_url('assets/img/team/Team6.jpg'); ?>" alt="">
               <h4>Harvey Revillas</h4>
               <p class="text-muted">Front-end Developer</p>
               <ul class="list-inline social-buttons">
@@ -294,7 +297,7 @@
           </div>
           <div class="col-sm-4">
             <div class="team-member">
-              <img class="mx-auto rounded-circle" src="assets/img/team/Team7.jpg" alt="">
+              <img class="mx-auto rounded-circle" src="<?php echo base_url('assets/img/team/Team7.jpg'); ?>" alt="">
               <h4>Jeanette Irish Sarza</h4>
               <p class="text-muted">Front-end Developer</p>
               <ul class="list-inline social-buttons">
@@ -318,8 +321,8 @@
           </div>
         </div>
     </section>
- 
-<!-- signin -->	
+	
+ <!-- Signin -->
   <div class="modal fade" id="demo-0" tabindex="-1">
     <div class="modal-dialog">
      <div class="modal-content">
@@ -333,9 +336,9 @@
         <div class="form-group">
               <label for="email">Email:</label>
               <input type="email" name="email" id="email" class="form-control required" placeholder="Enter email" required />
-              <br><label for="password">Password:</label>
+              <label for="password">Password:</label>
               <input type="password" id="pass" class="form-control" placeholder="Enter password" required />
-			  <br><input type="checkbox" onclick="myFunction()">Show Password
+			  <input type="checkbox" onclick="myFunction()">Show Password
         </div>
       </div>
        <div class="modal-footer">
@@ -345,38 +348,37 @@
      </div>
     </div>
   </div>
-
-<!-- signup -->
-<!-- [ Modal info ] -->
+  
+  <!-- Signup -->
+  <!-- [ Modal info ] -->
   <div class="modal fade" id="demo-1" tabindex="-1">
     <div class="modal-dialog">
      <div class="modal-content">
       <button type="button" class="close" data-dismiss="modal"><i class="icon-xs-o-md"></i></button>
       <div class="modal-header">
-      <h2 class="modal-title caps">Sign Up</h2>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h2 class="signinheader">Sign Up</h2>
+		<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
       </div>
       <div class="modal-body">
         <h1 style="text-align: center;">Welcome!</h1>
         <div class="form-group">
               <label for="email">Email:</label>
-              <input type="email" name="email" id="email" class="form-control required" placeholder="Enter email" required/>
-              <label for="password">Password:</label>
-              <input type="password" id="pass" class="form-control" placeholder="Enter password" required/>
-              <label for="company">Company Name:</label>
+              <input type="email" name="email" id="email1" class="form-control required" placeholder="Enter email" required/>
+              <br><label for="password">Password:</label>
+              <input type="password" id="pass1" class="form-control" placeholder="Enter password" required/>
+              <br><label for="company">Company Name:</label>
               <input type="text" id="company" class="form-control" placeholder="Enter company name" />
-              <label for="address">Address:</label>
+              <br><label for="address">Address:</label>
               <input type="text" id="address" class="form-control" placeholder="Enter address" />
-              <label for="zipcode">Zipcode:</label>
+              <br><label for="zipcode">Zipcode:</label>
               <input type="text" id="zipcode" class="form-control" placeholder="Enter zipcode" />
-              <label for="contact">Contact #:</label>
+              <br><label for="contact">Contact #:</label>
               <input type="text" id="contact" class="form-control" placeholder="Enter #" onkeypress='return event.charCode >= 48 && event.charCode <= 57' />
         </div>
       </div>
        <div class="modal-footer">
             <button type="button" class="btn btn-danger" data-dismiss="modal">&times;</button>
             <button type="button submit" class="btn btn-info" data-toggle="modal" onclick="return empty()">Next</button>
-            </button>
         </div>
      </div>
     </div>
@@ -388,8 +390,8 @@
      <div class="modal-content">
       <button type="button" class="close" data-dismiss="modal"><i class="icon-xs-o-md"></i></button>
       <div class="modal-header">
-      <h2 class="modal-title caps">Sign Up</h2>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h2 class="signinheader">Sign Up</h2>
+		<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
       </div>
       <div class="modal-body">
         <h1 style="text-align: center;">Subscription</h1>
@@ -405,27 +407,23 @@
         <label class="drinkcard-cc annual" for="annual"></label>
       </div></center>
       <div>
-        <img src="assets/img/paypal.png" height="100">      
+        <img src="<?php echo base_url('assets/img/paypal.png'); ?>" alt="" class="pay" height="100">      
         <h3 style="text-align: center;">Pay with PayPal</h3>  
       </div>
-
-      <div class="modal-body">
         <div class="form-group">
-          <div class="form-group">
-              <input type="email" name="email" id="paypalemail" class="form-control" placeholder="Email Address"/>
-              <input type="password" id="password" class="form-control" placeholder="Password" />
+              <input type="email" name="paypalemail" id="paypalemail" class="form-control" placeholder="Email Address"/>
+              <input type="password" id="paypalpass" name="paypalpass" class="form-control" placeholder="Password" />
         </div>
-        </div>
-      </div>
       </div>
        <div class="modal-footer">
             <button type="button" class="btn btn-danger" data-dismiss="modal">&times;</button>
             <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#demo-1" data-dismiss="modal">Back</button>
-            <button type="button" class="btn btn-success" data-toggle="modal"onclick="emptyPayment()">Submit</button>
+            <button type="button" class="btn btn-success" data-toggle="modal" onclick="emptyPayment()">Submit</button>
         </div>
      </div>
     </div>
-	    
+  </div>
+  
     <!-- Footer -->
     <footer>
       <div class="container">
@@ -467,17 +465,17 @@
     </footer>
 	
     <!-- Bootstrap core JavaScript -->
-    <script src="assets/vendor/jquery/jquery.js"></script>
-    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
-
+    <script type="text/javascript" src="<?php echo base_url('assets/vendor/jquery/jquery.js'); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('assets/vendor/bootstrap/js/bootstrap.bundle.js'); ?>"></script>
+	
     <!-- Plugin JavaScript -->
-    <script src="assets/vendor/jquery-easing/jquery.easing.js"></script>
-
+    <script type="text/javascript" src="<?php echo base_url('assets/vendor/jquery-easing/jquery.easing.js'); ?>"></script>
+	
     <!-- Custom scripts for this template -->
-    <script src="assets/js/agency.js"></script>
-    <script src="assets/js/signin.js"></script>
-    <script src="assets/js/signup.js"></script>
-
+    <script type="text/javascript" src="<?php echo base_url('assets/js/agency.js'); ?>"></script>
+	<script type="text/javascript" src="<?php echo base_url('assets/js/sign.js'); ?>"></script>
+	
+	
   </body>
 
 </html>
