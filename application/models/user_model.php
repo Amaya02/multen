fe0ac16f4fa6c44b49dd6e52735a0388d84f6396
CandyAmayaLelis <34389354+Amaya02@users.noreply.github.com>
2018-02-13 21:13:20 +0800

    added model

---
+++ application/models/user_model.php
@@ -0,0 +1,72 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class user_model extends CI_Model {
+    
+	
+	// Constructor
+	public function __construct()
+	{
+		parent::__construct();		
+	}
+	
+	public function validateadmin(){
+		// grab user input
+		$email = $this->security->xss_clean($this->input->post('email'));
+		$password = $this->security->xss_clean($this->input->post('password'));
+		
+		//prep the query
+		$this->db->where('email',$email);
+		$this->db->where('password',$password);
+		
+		//run the query
+		$query = $this->db->get('admin');
+		
+		// check if there are any results
+		if($query->num_rows()==1){
+			//if there is a user, then create session data
+			$row = $query->row();
+			$data = array(
+						'adminid' => $row->adminid,
+						'username' => $row->username,
+						'email' => $row->email,
+						'validated' => true);
+			$this->session->set_userdata($data);
+			return true;
+		}
+		//if the previous process did not validate
+		//then return false
+		return false;
+	}
+	
+	public function validateuser(){
+		// grab user input
+		$email = $this->security->xss_clean($this->input->post('email'));
+		$password = $this->security->xss_clean($this->input->post('password'));
+		
+		//prep the query
+		$this->db->where('email',$email);
+		$this->db->where('password',$password);
+		
+		//run the query
+		$query = $this->db->get('users');
+		
+		// check if there are any results
+		if($query->num_rows()==1){
+			//if there is a user, then create session data
+			$row = $query->row();
+			$data = array(
+						'userid' => $row->userid,
+						'companyname' => $row->companyname,
+						'email' => $row->email,
+						'validated' => true);
+			$this->session->set_userdata($data);
+			return true;
+		}
+		//if the previous process did not validate
+		//then return false
+		return false;
+	}
+}
+
+?>
