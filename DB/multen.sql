3081c9d951d47816add0269e8f6364e554810e05
thomas-nii <35361333+thomas-nii@users.noreply.github.com>
2018-02-23 19:31:26 +0800

    Update multen.sql

--- DB/multen.sql
+++ DB/multen.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3307
--- Generation Time: Feb 17, 2018 at 03:34 AM
--- Server version: 10.1.29-MariaDB
--- PHP Version: 7.2.0
+-- Host: 127.0.0.1
+-- Generation Time: Feb 23, 2018 at 12:22 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -35,6 +35,49 @@ CREATE TABLE `admin` (
   `username` varchar(29) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`adminid`, `email`, `password`, `username`) VALUES
+(0, 'adminamaya@yahoo.com', 'secret', 'amayaadmin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bill`
+--
+
+CREATE TABLE `bill` (
+  `billid` int(50) NOT NULL,
+  `subscription` varchar(50) NOT NULL,
+  `amount` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bill`
+--
+
+INSERT INTO `bill` (`billid`, `subscription`, `amount`) VALUES
+(1, 'trial', 0),
+(2, 'monthly', 10),
+(3, 'quarterly', 55),
+(4, 'annually', 96);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `config`
+--
+
+CREATE TABLE `config` (
+  `configid` int(50) NOT NULL,
+  `userid` int(50) NOT NULL,
+  `websitename` varchar(50) NOT NULL,
+  `databasename` varchar(50) NOT NULL,
+  `template` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- --------------------------------------------------------
 
 --
@@ -42,10 +85,17 @@ CREATE TABLE `admin` (
 --
 
 CREATE TABLE `users` (
-  `userid` int(10) NOT NULL,
-  `email` varchar(29) NOT NULL,
-  `password` varchar(29) NOT NULL,
-  `companyname` varchar(29) NOT NULL
+  `userid` int(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `companyname` varchar(50) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `state` varchar(50) NOT NULL,
+  `zipcode` varchar(50) NOT NULL,
+  `cnumber` varchar(50) NOT NULL,
+  `conemail` varchar(50) NOT NULL,
+  `billid` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -59,10 +109,38 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`adminid`);
 
 --
+-- Indexes for table `bill`
+--
+ALTER TABLE `bill`
+  ADD PRIMARY KEY (`billid`);
+
+--
+-- Indexes for table `config`
+--
+ALTER TABLE `config`
+  ADD PRIMARY KEY (`configid`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`userid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `config`
+--
+ALTER TABLE `config`
+  MODIFY `configid` int(50) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userid` int(20) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
