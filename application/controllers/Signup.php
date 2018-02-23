a4a15cdad7b8f9cb482d9e625a37b548de0072f4
Harvey Revillas <keikirishima891@gmail.com>
2018-02-23 18:56:06 +0800

    Add Signup.php

---
+++ application/controllers/Signup.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Signup extends CI_Controller {
+
+	function __construct(){
+		parent::__construct();
+
+	}
+	
+	public function index()
+	{
+		$this->load->view('signup');
+	}
+	
+	
+}
+
+?>
+	
