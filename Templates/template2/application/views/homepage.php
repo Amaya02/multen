a157c9d4b9b240f5e49dbaf43a1e98f9975fd112
thomas-nii <deanjagmis@gmail.com>
2018-03-09 20:22:12 +0800

    Update homepage.php

--- Templates/template2/application/views/homepage.php
+++ Templates/template2/application/views/homepage.php
@@ -3,10 +3,10 @@
 
   <head>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
 
 	<title>
 		<?php foreach($posts as $post){?>
@@ -15,13 +15,13 @@
 	</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="<?php echo base_url('assets/vendor/bootstrap/css/bootstrap.css'); ?>" rel="stylesheet">
+    <link href="<?php echo base_url('assets/vendor/bootstrap/css/bootstrap.css'); ?>" rel="stylesheet" />
 	
 
     <!-- Custom styles for this template -->
-    <link href="<?php echo base_url('assets/css/full-slider.css'); ?>" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/signin.css'); ?>">
-	<link href="<?php echo base_url('assets/css/style.css'); ?>" rel="stylesheet">
+    <link href="<?php echo base_url('assets/css/full-slider.css'); ?>" rel="stylesheet" />
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/signin.css'); ?>" />
+	<link href="<?php echo base_url('assets/css/style.css'); ?>" rel="stylesheet" />
 
   </head>
 
@@ -40,7 +40,7 @@
 <!-- Navigation -->
 		<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
        <div class="container">
-        <img src="<?php echo base_url('assets/img/logos/logo.jpg'); ?>" alt="Logo"></a>
+        <img src="<?php echo base_url('assets/img/logos/logo.jpg'); ?>" alt="Logo">
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
           <i class="fa fa-bars"></i>
@@ -106,9 +106,9 @@
     <form action='<?php base_url();?>login' method='post' name='process' autocomplete="off">
               <label for="email">Email:</label>
               <input type="email" name="email" id="email" class="form-control required" placeholder="Enter email" required />
-              <br><label for="password">Password:</label>
+              <br/><label for="password">Password:</label>
               <input type="password" id="password" name="password" class="form-control" placeholder="Enter password" required />
-        <br><input type="checkbox" onclick="myFunction()">Show Password
+        <br/><input type="checkbox" onclick="myFunction()" />Show Password
         </div>
       </div>
        <div class="modal-footer">
